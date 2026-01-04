156f04dadd6163f4ab5235eef88ccb805a48b621
Tretiner <alexandirina@inbox.ru>
2026-01-04 17:53:44 +0500

    up to tailwind v4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Inter } from "next/font/google";
+import "./globals.css"; // Путь должен быть правильным!
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin", "cyrillic"] });
 
 export const metadata: Metadata = {
   title: "MedSafety",
@@ -12,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased bg-brand-bg">
+    <html lang="ru">
+      <body className={cn("min-h-screen bg-brand-bg", inter.className)}>
         {children}
       </body>
     </html>
