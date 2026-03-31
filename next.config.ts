7815636a6285171b53d268cc144bc0e6a33a6b71
Tretiner <alexandirina@inbox.ru>
2026-03-31 16:25:32 +0000

    server migration

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
     "127.0.0.1.nip.io", // Telegram widget (run http://localhost:80)
     "auth.medincident.dreyn-drafts.ru",
     "id-medincident.ulbwa.bombomeow.ru",
+    "zitadel-medincident.ulbwa.bombomeow.ru",
   ],
 
   experimental: {
